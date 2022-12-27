970cbc050588789a9c205eccc8dc0d24260b8498
katenych <kat.shcherbak@gmail.com>
2022-12-27 10:09:47 -0800

    Errors corrected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        int [] arr1 = {1,2,3,4,5,6,7,8,9,10};
-        int [] arr2 = {1,2,3,4,5,6,7,8,9,10};
+        int [] arr1 = new int[7];
+        int [] arr2 = new int[7];
+
+
 
         Random random = new Random();
         String stringArr1 = " ";
@@ -17,17 +20,16 @@ public class Main {
             stringArr2 += " " + (arr2[i] = random.nextInt(10));
 
         }
-
-        System.out.println(stringArr1);
-        System.out.println(stringArr2);
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
 
         int count = 0;
-        for(int i=0; i<arr1.length -1; i++){
-            if(arr1[i] == arr2[i]) count++; {
-
-            }
-
+        for(int i=0; i<arr1.length; i++){
+            if (arr1[i] == arr2[i]) count++;
         }
+
         System.out.print("Кількість збігів: " + count);
     }
 }
